4ecce99bea06c7111df641e46e3546c7f0a9bcac
Dusan Aksic <aksacode@gmail.com>
2021-10-19 15:25:46 +0200

    making grid

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -1,19 +1,35 @@
 import React from "react";
-import { Button, StyleSheet, Text, View } from "react-native";
-
+import { Button, FlatList, StyleSheet, Text, View } from "react-native";
+import { CATEGORIES } from "../data/dummy-data";
+const renderGridItemFunction = (itemData) => {
+  return (
+    <View style={styles.gridItem}>
+      <Text>{itemData.item.title}</Text>
+    </View>
+  );
+};
 const CategoriesScreen = (props) => {
   console.log(props);
   return (
-    <View style={styles.screen}>
-      <Text>Categories screen</Text>
-      <Text>Surround me, it's easy</Text>
-      <Button
-        title="Go on Meals"
-        onPress={() => {
-          props.navigation.navigate({ routeName: "CategoryMeals" });
-        }}
-      />
-    </View>
+    <FlatList
+      data={CATEGORIES}
+      renderItem={renderGridItemFunction}
+      numColumns={2}
+    />
+    // <View style={styles.screen}>
+    //   <Text>Categories screen</Text>
+    //   <Text>Surround me, it's easy</Text>
+    //   <Button
+    //     title="Go on Meals"
+    //     onPress={() => {
+    //       props.navigation.navigate({ routeName: "CategoryMeals" });
+    //       // .push tehnicki refreshuje screen
+    //       // .goBack nas vraca korak nazad .pop valjda isto
+    //       // .popToTop vraca nas na root screen
+    //       // .replace nam ne daje opciju da se vratimo strelicom (uzima samo string do skrina definisanog u mealNavigator)
+    //     }}
+    //   />
+    // </View>
   );
 };
 
@@ -24,5 +40,10 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
+  },
+  gridItem: {
+    flex: 1,
+    margin: 15,
+    height: 150,
   },
 });
