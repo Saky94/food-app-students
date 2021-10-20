54bcdd147deffdfa2634237693afd5c91a2fe9cf
Dusan Aksic <aksacode@gmail.com>
2021-10-20 08:37:23 +0200

    using of params and getParam

--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,11 +1,14 @@
 import React from "react";
 import { StyleSheet, Text, View, Button } from "react-native";
-
+import { CATEGORIES } from "../data/dummy-data";
 const CategoryMealsScreen = (props) => {
+  const catId = props.navigation.getParam("categoryId");
+  const selectedCategory = CATEGORIES.find((category) => category.id === catId);
   return (
     <View style={styles.screen}>
       <Text>Category meals screen</Text>
       <Text>Hello darkness my old friend</Text>
+      <Text>{selectedCategory.title}</Text>
       <Button
         title="Go on detail"
         onPress={() => {
