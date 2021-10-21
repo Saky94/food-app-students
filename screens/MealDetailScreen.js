ea9b919f29f7572a03e2970be2bcc2e9b90bfd1f
Dusan Aksic <aksacode@gmail.com>
2021-10-21 13:38:37 +0200

    headerbutton

--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { StyleSheet, Text, View, Button } from "react-native";
 import { MEALS } from "../data/dummy-data";
+import { HeaderButtons, Item } from "react-navigation-header-buttons";
+import CustomHeaderButton from "../components/CustomHeaderButton";
 const MealDetailScreen = (props) => {
   const mealId = props.navigation.getParam("mealId");
   console.log(mealId);
@@ -14,9 +16,20 @@ const MealDetailScreen = (props) => {
 };
 MealDetailScreen.navigationOptions = (navigationData) => {
   const mealId = navigationData.navigation.getParam("mealId");
-  const selectedMeal = MEALS.find((meal) => meal === mealId);
+  const selectedMeal = MEALS.find((meal) => meal.id === mealId);
   return {
     headerTitle: selectedMeal.title,
+    headerRight: () => (
+      <HeaderButtons HeaderButtonComponent={CustomHeaderButton}>
+        <Item
+          title="star"
+          iconName="ios-star"
+          onPress={() => {
+            console.log("right icon");
+          }}
+        />
+      </HeaderButtons>
+    ),
   };
 };
 export default MealDetailScreen;
