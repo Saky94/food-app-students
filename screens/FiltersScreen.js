37319d76bc585ce8a66ce50354baeae9a98e5aac
Dusan Aksic <aksacode@gmail.com>
2021-10-23 13:28:09 +0200

    switch adding

--- screens/FiltersScreen.js
+++ screens/FiltersScreen.js
@@ -1,11 +1,48 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+import { StyleSheet, Text, View, Switch } from "react-native";
 import { HeaderButtons, Item } from "react-navigation-header-buttons";
 import CustomHeaderButton from "../components/CustomHeaderButton";
-const FiltersScreen = () => {
+const FilterSwitch = (props) => {
   return (
-    <View>
-      <Text></Text>
+    <View style={styles.filterContainer}>
+      <Text>{props.label}</Text>
+      <Switch
+        trackColor={{ true: "yellow" }}
+        thumbColor="blue"
+        value={props.state}
+        onValueChange={props.onChange}
+      />
+    </View>
+  );
+};
+const FiltersScreen = () => {
+  const [isGlutenFree, setIsGlutenFree] = useState(false);
+  const [isLactoseFree, setIsLactoseFree] = useState(false);
+  const [isVegan, setIsVegan] = useState(false);
+  const [isVegetarian, setIsVegetarian] = useState(false);
+  return (
+    <View style={styles.screen}>
+      <Text style={styles.title}>Available filters</Text>
+      <FilterSwitch
+        label="Gluten-free"
+        state={isGlutenFree}
+        onChange={(changeValue) => setIsGlutenFree(changeValue)}
+      />
+      <FilterSwitch
+        label="Lactose-free"
+        state={isLactoseFree}
+        onChange={(changeValue) => setIsLactoseFree(changeValue)}
+      />
+      <FilterSwitch
+        label="Vegan"
+        state={isVegan}
+        onChange={(changeValue) => setIsVegan(changeValue)}
+      />
+      <FilterSwitch
+        label="Vegetarian"
+        state={isVegetarian}
+        onChange={(changeValue) => setIsVegetarian(changeValue)}
+      />
     </View>
   );
 };
@@ -27,4 +64,19 @@ FiltersScreen.navigationOptions = (navData) => {
     ),
   };
 };
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  screen: { flex: 1, alignItems: "center" },
+  title: {
+    fontFamily: "open-sans-bold",
+    fontSize: 20,
+    margin: 20,
+    textAlign: "center",
+  },
+  filterContainer: {
+    marginVertical: 10,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    width: "80%",
+  },
+});
