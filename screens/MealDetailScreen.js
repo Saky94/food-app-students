b319d3acb1e414ea971861ba22834ba284075fbe
Dusan Aksic <aksacode@gmail.com>
2021-10-23 12:44:12 +0200

    making meal detail screen

--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,17 +1,45 @@
 import React from "react";
-import { StyleSheet, Text, View, Button } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  ScrollView,
+  Image,
+} from "react-native";
 import { MEALS } from "../data/dummy-data";
 import { HeaderButtons, Item } from "react-navigation-header-buttons";
 import CustomHeaderButton from "../components/CustomHeaderButton";
+import DefaultText from "../components/DefaultText";
+const ListItem = (props) => {
+  return (
+    <View style={styles.listItem}>
+      <DefaultText>{props.children}</DefaultText>
+    </View>
+  );
+};
 const MealDetailScreen = (props) => {
   const mealId = props.navigation.getParam("mealId");
-  console.log(mealId);
+  // console.log(mealId);
   const selectedMeal = MEALS.find((meal) => meal.id === mealId);
-  console.log(selectedMeal);
+  // console.log(selectedMeal);
   return (
-    <View style={styles.screen}>
-      <Text>Meal title is {selectedMeal.title}</Text>
-    </View>
+    <ScrollView>
+      <Image source={{ uri: selectedMeal.imageUrl }} style={styles.image} />
+      <View style={styles.details}>
+        <DefaultText>{selectedMeal.duration}min</DefaultText>
+        <DefaultText>{selectedMeal.complexity}</DefaultText>
+        <DefaultText>{selectedMeal.affordability}</DefaultText>
+      </View>
+      <Text style={styles.title}>Ingredients</Text>
+      {selectedMeal.ingredients.map((ingredient) => (
+        <ListItem key={ingredient}>{ingredient}</ListItem>
+      ))}
+      <Text style={styles.title}>Steps</Text>
+      {selectedMeal.steps.map((step) => (
+        <ListItem key={step}>{step}</ListItem>
+      ))}
+    </ScrollView>
   );
 };
 MealDetailScreen.navigationOptions = (navigationData) => {
@@ -25,7 +53,7 @@ MealDetailScreen.navigationOptions = (navigationData) => {
           title="star"
           iconName="ios-star"
           onPress={() => {
-            console.log("right icon");
+            // console.log("right icon");
           }}
         />
       </HeaderButtons>
@@ -35,9 +63,22 @@ MealDetailScreen.navigationOptions = (navigationData) => {
 export default MealDetailScreen;
 
 const styles = StyleSheet.create({
-  screen: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
+  image: { width: "100%", height: 200 },
+  details: {
+    flexDirection: "row",
+    padding: 15,
+    justifyContent: "space-around",
+  },
+  title: {
+    fontFamily: "open-sans-bold",
+    fontSize: 22,
+    textAlign: "center",
+  },
+  listItem: {
+    marginVertical: 10,
+    marginHorizontal: 20,
+    borderColor: "blue",
+    borderWidth: 1,
+    padding: 10,
   },
 });
