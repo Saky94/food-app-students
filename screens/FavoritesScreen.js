724df2d7275fcbf9876b1408a21e6c4cf09cc9ff
Dusan Aksic <aksacode@gmail.com>
2021-10-26 12:53:04 +0200

    added text for empty favorites

--- screens/FavoritesScreen.js
+++ screens/FavoritesScreen.js
@@ -7,7 +7,13 @@ import MealList from "../components/MealList";
 import { useSelector } from "react-redux";
 const FavoritesScreen = (props) => {
   const favMeals = useSelector((state) => state.meals.favoriteMeals);
-
+  if (favMeals.length === 0 || !favMeals) {
+    return (
+      <View style={styles.content}>
+        <Text>There is no favorite meals added.</Text>
+      </View>
+    );
+  }
   return <MealList listData={favMeals} navigation={props.navigation} />;
 };
 
@@ -30,4 +36,10 @@ FavoritesScreen.navigationOptions = (navData) => {
 
 export default FavoritesScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  content: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
