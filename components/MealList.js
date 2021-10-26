fce03a63702845744517323b6fd31ccfca789b72
Dusan Aksic <aksacode@gmail.com>
2021-10-26 12:46:40 +0200

    fav star changes

--- components/MealList.js
+++ components/MealList.js
@@ -1,9 +1,13 @@
 import React from "react";
 import { StyleSheet, Text, View, FlatList } from "react-native";
 import MealItem from "./MealItem";
-
+import { useSelector } from "react-redux";
 const MealList = (props) => {
+  const favoriteMeals = useSelector((state) => state.meals.favoriteMeals);
   const renderMealItem = (itemData) => {
+    const isFavorite = favoriteMeals.some(
+      (meal) => meal.id === itemData.item.id
+    );
     return (
       <MealItem
         title={itemData.item.title}
@@ -16,6 +20,7 @@ const MealList = (props) => {
             params: {
               mealId: itemData.item.id,
               mealTitle: itemData.item.title,
+              isFav: isFavorite,
             },
           });
         }}
